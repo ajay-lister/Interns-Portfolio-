dea3d6579d1b172c1ba8106b59068008778026fc
ajay-lister <ajayshankar.ag@listertechnologies.com>
2021-08-02 15:46:46 +0530

    encryption update

--- Database layer/interns.js
+++ Database layer/interns.js
@@ -1,85 +1,89 @@
-const express = require('express')
-const router = express.Router()
-const { loginschema } = require('./interntable')
-const { internschema } = require('./interntable')
-const con = require('./dbconnection')
-const bodyParser = require('body-parser')
-const alert = require('alert')
+const express = require("express");
+const router = express.Router();
+const { loginschema } = require("./interntable");
+const { internschema } = require("./interntable");
+const con = require("./dbconnection");
+const bodyParser = require("body-parser");
+const alert = require("alert");
+const bcrypt = require("bcrypt");
+const Cryptr = require("cryptr");
 
-const app = express()
+cryptr = new Cryptr("abc");
 
-con.on('open', () => {
-    console.log("Connected....")
-})
+const app = express();
+con.on("open", () => {
+  console.log("Connected....");
+});
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
 var detail = [];
 
-router.post('/validate', async (req, res) => {
-    var un = req.body.Name
-    var pwd = req.body.Password
-    if (un == "admin" && pwd == "admin") {
-        res.redirect('/demoprofile')
-    }
-    else {
-        loginschema.findOne({ Name: un, Password: pwd }, function (err, user) {
-            if (err) console.log(err);
-            if (!user) alert('incorrect username/password');
-            else {
-                internschema.findOne({ Name: un }, function (err, intern) {
-                    if (err) console.log(err);
-                    console.log(intern)
-                    // detail = intern;
-                    res.render('../View layer/demoprofile', {
-                        "details": intern
-                    })
-                })
-                // res.redirect('/load')
-                // res.redirect('/profile',)
-            }
-        })
-    }
-})
+router.post("/validate", async (req, res) => {
+  var un = req.body.Name;
+  var pwd = req.body.Password;
 
-router.get('/fetchall', async (req, res) => {
-    internschema.find({}, function (err, intern) {
-        if (err) console.log(err);
-        res.render('../View layer/Internslist.ejs', {
-            "interns": intern
-        })
-    })
-})
-
-
-
-router.post('/', async (req, res) => {
-    const login = new loginschema({
-        _id: req.body.Eid,
-        Name: req.body.Name,
-        Password: req.body.Password
-    })
-
-    const intern = new internschema({
-        _id: req.body.Eid,
-        Name: req.body.Name,
-        Domain: req.body.Domain,
-        Description: req.body.Description,
-        Mail: req.body.Mail
+  if (un == "admin" && pwd == "admin") {
+    res.redirect("/demoprofile");
+  } else {
+    loginschema.findOne({ Name: un }, function (err, user) {
+      if (err) console.log(err);
+      if (cryptr.decrypt(user.Password) != pwd || !user)
+        alert("incorrect username/password");
+      else {
+        internschema.findOne({ Name: un }, function (err, intern) {
+          if (err) console.log(err);
+          console.log(intern);
+          // detail = intern;
+          res.render("../View layer/demoprofile", {
+            details: intern,
+          });
+        });
+        // res.redirect('/load')
+        // res.redirect('/profile',)
+      }
     });
+  }
+});
 
-    try {
-        const i1 = await intern.save()
-        const i2 = await login.save()
-        res.redirect('/Homepage')
-    }
-    catch (err) {
-        res.send("Error")
-    }
-})
+router.get("/fetchall", async (req, res) => {
+  internschema.find({}, function (err, intern) {
+    if (err) console.log(err);
+    res.render("../View layer/Internslist.ejs", {
+      interns: intern,
+    });
+  });
+});
 
+router.post("/", async (req, res) => {
+  // let pwd = await bcrypt.hash(req.body.Password, 10);
+
+  const pwd = cryptr.encrypt(req.body.Password);
+
+  const login = new loginschema({
+    _id: req.body.Eid,
+    Name: req.body.Name,
+    Password: pwd,
+  });
+
+  const intern = new internschema({
+    _id: req.body.Eid,
+    Name: req.body.Name,
+    Domain: req.body.Domain,
+    Description: req.body.Description,
+    Mail: req.body.Mail,
+  });
+
+  try {
+    const i1 = await intern.save();
+    const i2 = await login.save();
+    res.redirect("/Homepage");
+  } catch (err) {
+    res.send("Error");
+  }
+});
 
 module.exports = {
-    router: router,
-    details: detail
-}
+  router: router,
+  details: detail,
+};
